2d6f2cbca6ce524d6d3fe8e8626bd8780481ef98
krishna sadaivam <chander.krishna@gmail.com>
2014-10-15 22:01:53 -0700

    simple node server

---
+++ js/nodeServer.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+res.setHeader('Connection', 'Transfer-Encoding');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Transfer-Encoding', 'chunked');
+
+res.write("super ");
+setTimeout(function() {
+res.end("hello world");
+}, 5000);
+var i;
+for(i = 0; i < 10; i++) {
+res.write("superstart" + i + " ");
+}
+}).listen(21213);
