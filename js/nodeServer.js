99128c1747b101131ae3584aa79aecde36843bd1
krishna sadaivam <chander.krishna@gmail.com>
2014-10-31 15:22:15 -0700

    improve logging in nodejs server

--- js/nodeServer.js
+++ js/nodeServer.js
@@ -1,7 +1,13 @@
 var http = require('http');
 
 http.createServer(function(req, res) {
+	var result = JSON.stringify({hello: "hello world"});
+
 	res.setHeader('Access-Control-Allow-Origin','http://localhost:21213');
     res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify({hello: "hello world"}));
+    res.end(result);
+    console.log("sending response: " + result);
+    console.log("==========END============");
 }).listen(21214);
+
+console.log("Running server at http://localhost:21214");
