cb852948bbcff0f90aa2258b052c52e556849d1f
krishna sadaivam <chander.krishna@gmail.com>
2014-11-12 17:53:39 -0800

    remove text file and add chrome webdrive ui tests.

--- test/ui.test.js
+++ test/ui.test.js
@@ -1,13 +1,18 @@
-var webdriver = require('selenium-webdriver\\index.js');
+var webdriver = require('selenium-webdriver/index.js');
 
 var driver = new webdriver.Builder().
    withCapabilities(webdriver.Capabilities.chrome()).
    build();
 
-driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
-driver.findElement(webdriver.By.name('btnG')).click();
+driver.get('https://portal.azure.com');
+driver.findElement(webdriver.By.name('login')).sendKeys('ksadasiv@live.com');
+driver.findElement(webdriver.By.id('cred_continue_button')).click();
+	driver.findElement(webdriver.By.name('passwd')).sendKeys('love21bangalore');
+	driver.findElement(webdriver.By.name('SI')).click();
+	
 driver.wait(function() {
+
+
  return driver.getTitle().then(function(title) {
    return title === 'webdriver - Google Search';
  });
